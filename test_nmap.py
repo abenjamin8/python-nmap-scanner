0ca1fe8119ebfb85b6dc466214bc7d3240d3020c
abenjamin8 <46075334+abenjamin8@users.noreply.github.com>
2026-01-09 19:54:39 +0000

    test_nmap.py

---
+++ test_nmap.py
@@ -0,0 +1,14 @@
+import nmap
+
+scanner = nmap.PortScanner()
+scanner.scan('127.0.0.1', '1-1024')
+
+for host in scanner.all_hosts():
+    print(f"Host: {host}")
+    print(f"State: {scanner[host].state()}")
+
+    for proto in scanner[host].all_protocols():
+        ports = scanner[host][proto].keys()
+        for port in ports:
+            state = scanner[host][proto][port]['state']
+            print(f"  {proto} port {port}: {state}")
